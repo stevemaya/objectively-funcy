bee3e88de37be512e9b4b38cc5a5aa43ee7ec705
Steve Maya <steve.maya@codeimmersives.com>
2020-10-15 14:01:57 -0500

    up to marry

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 // Your code here.
 
+const getFirstName = function (person) {
+  return person.firstName;
+};
 
+const getLastName = function (person) {
+  return person.lastName; 
+};
 
+const getFullName = function (person) {
+  return person.firstName + ' ' + person.lastName;
+};
 
+const setFirstName = function (person, name) {
+  return person.firstName == name;
+};
+
+const setAge = function (person, age) {
+  return person.age == age;
+};
+
+const giveBirthday = function (person){
+  if ("age" in person){
+    person.age++;
+  } else {
+    person.age = 1;
+  }
+};
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
